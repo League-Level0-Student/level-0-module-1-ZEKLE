612f2a432befa48bfae1d3bf298e4d541a512e16
League Student <league@iMac-22.attlocal.net>
2019-10-03 15:52:23 -0700

    Checkpoint

--- src/_99_extra/checkpoint.java
+++ src/_99_extra/checkpoint.java
@@ -5,6 +5,11 @@ import javax.swing.JOptionPane;
 public class checkpoint {
 public static void main(String[] args) {
 	String word = JOptionPane.showInputDialog(null, "give me a word!");
-	for(int i = 0; i < 1; i++);
-}
+	String num = JOptionPane.showInputDialog("Give me a number");
+	int value = Integer.parseInt(num);
+	for(int i = 0; i < value; i++) {
+		System.out.println(word);
+		
+		}
+	}
 }
